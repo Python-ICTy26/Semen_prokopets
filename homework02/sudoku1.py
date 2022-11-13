4bec0d0b466cacc2b063bcecc3010a8252050611
Сема <112980469+Semen4ikk@users.noreply.github.com>
2022-11-13 19:47:46 +0300

    Add files via upload

---
+++ homework02/sudoku1.py
@@ -0,0 +1,233 @@
+import random
+from typing import List, Optional, Set, Tuple
+
+
+def read_sudoku(filename: str) -> List[List[str]]:
+    """ Прочитать Судоку из указанного файла """
+    digits = [c for c in open(filename).read() if c in "123456789."]
+    grid = group(digits, 9)
+    return grid
+
+
+def display(grid: List[List[str]]) -> None:
+    """Вывод Судоку """
+    width = 2
+    line = "+".join(["-" * (width * 3)] * 3)
+    for row in range(9):
+        print("".join(grid[row][col].center(width) + ("|" if str(col) in "25" else "") for col in range(9)))
+        if str(row) in "25":
+            print(line)
+    print()
+
+
+def group(values: List[str], n: int) -> List[List[str]]:
+    """
+    Сгруппировать значения values в список, состоящий из списков по n элементов
+
+    >>> group([1,2,3,4], 2)
+    [[1, 2], [3, 4]]
+    >>> group([1,2,3,4,5,6,7,8,9], 3)
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    """
+    return [values[i : i + n] for i in range(0, len(values), n)]
+
+
+def get_row(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
+    """Возвращает все значения для номера строки, указанной в pos
+
+    >>> get_row([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
+    ['1', '2', '.']
+    >>> get_row([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']], (1, 0))
+    ['4', '.', '6']
+    >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
+    ['.', '8', '9']
+    """
+    return grid[pos[0]]
+
+
+def get_col(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
+    """Возвращает все значения для номера столбца, указанного в pos
+
+    >>> get_col([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
+    ['1', '4', '7']
+    >>> get_col([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']], (0, 1))
+    ['2', '.', '8']
+    >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
+    ['3', '6', '9']
+    """
+    result = []
+    for row in grid:
+        result.append(row[pos[1]])
+    return result
+
+
+def get_block(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
+    """Возвращает все значения из квадрата, в который попадает позиция pos
+
+    >>> grid = read_sudoku('puzzle1.txt')
+    >>> get_block(grid, (0, 1))
+    ['5', '3', '.', '6', '.', '.', '.', '9', '8']
+    >>> get_block(grid, (4, 7))
+    ['.', '.', '3', '.', '.', '1', '.', '.', '6']
+    >>> get_block(grid, (8, 8))
+    ['2', '8', '.', '.', '.', '5', '.', '7', '9']
+    """
+    block = []
+    for rowind in range(pos[0] // 3 * 3, pos[0] // 3 * 3 + 3):
+        for colind in range(pos[1] // 3 * 3, pos[1] // 3 * 3 + 3):
+            block.append(grid[rowind][colind])
+    return block
+
+
+def find_empty_positions(grid: List[List[str]]) -> Optional[Tuple[int, int]]:
+    """Найти первую свободную позицию в пазле
+
+    >>> find_empty_positions([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']])
+    (0, 2)
+    >>> find_empty_positions([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']])
+    (1, 1)
+    >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
+    (2, 0)
+    """
+    for rowind in range(0, len(grid)):
+        for colind in range(0, len(grid)):
+            if grid[rowind][colind] == ".":
+                return rowind, colind
+    return None
+
+
+def find_possible_values(grid: List[List[str]], pos: Tuple[int, int]) -> Set[str]:
+    """Вернуть множество возможных значения для указанной позиции
+
+    >>> grid = read_sudoku('puzzle1.txt')
+    >>> values = find_possible_values(grid, (0,2))
+    >>> values == {'1', '2', '4'}
+    True
+    >>> values = find_possible_values(grid, (4,7))
+    >>> values == {'2', '5', '9'}
+    True
+    """
+    numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+    lstrow = get_row(grid, pos)
+
+    for element in lstrow:
+        if element in numbers:
+            numbers.remove(element)
+
+    lstcol = get_col(grid, pos)
+
+    for element in lstcol:
+        if element in numbers:
+            numbers.remove(element)
+
+    lstblock = get_block(grid, pos)
+
+    for element in lstblock:
+        if element in numbers:
+            numbers.remove(element)
+    return numbers
+
+
+def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
+    """ Решение пазла, заданного в grid """
+    """ Как решать Судоку?
+        1. Найти свободную позицию
+        2. Найти все возможные значения, которые могут находиться на этой позиции
+        3. Для каждого возможного значения:
+            3.1. Поместить это значение на эту позицию
+            3.2. Продолжить решать оставшуюся часть пазла
+
+    >>> grid = read_sudoku('puzzle1.txt')
+    >>> solve(grid)
+    [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
+    """
+
+    empty_position = find_empty_positions(grid)
+    if not empty_position:
+        return grid
+    rowind, colind = empty_position
+    grid_new = []
+    for row in grid:
+        grid_new.append(row.copy())
+    for number in find_possible_values(grid_new, empty_position):
+        grid_new[rowind][colind] = number
+        solution = solve(grid_new)
+        if solution is not None:
+            return solution
+    return None
+
+
+def check_solution(solution: List[List[str]]) -> bool:
+    """ Если решение solution верно, то вернуть True, в противном случае False """
+    # TODO: Add doctests with bad puzzles
+    for row in solution:
+        numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+        for element_row in row:
+            if element_row in numbers:
+                numbers.remove(element_row)
+        if len(numbers) != 0:
+            return False
+    for colind in range(0, 9):
+        numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+        for element_col in get_col(solution, (0, colind)):
+            if element_col in numbers:
+                numbers.remove(element_col)
+        if len(numbers) != 0:
+            return False
+    for rowind in range(0, 6, 3):
+        for colind in range(0, 6, 3):
+            numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+            for element_block in get_block(solution, (rowind, colind)):
+                if element_block in numbers:
+                    numbers.remove(element_block)
+            if len(numbers) != 0:
+                return False
+    return True
+
+
+def generate_sudoku(N: int) -> List[List[str]]:
+    """Генерация судоку заполненного на N элементов
+
+    >>> grid = generate_sudoku(40)
+    >>> sum(1 for row in grid for e in row if e == '.')
+    41
+    >>> solution = solve(grid)
+    >>> check_solution(solution)
+    True
+    >>> grid = generate_sudoku(1000)
+    >>> sum(1 for row in grid for e in row if e == '.')
+    0
+    >>> solution = solve(grid)
+    >>> check_solution(solution)
+    True
+    >>> grid = generate_sudoku(0)
+    >>> sum(1 for row in grid for e in row if e == '.')
+    81
+    >>> solution = solve(grid)
+    >>> check_solution(solution)
+    True
+    """
+    grid = solve([["."] * 9 for _ in range(0, 9)])
+    if grid is not None:
+        k_empty = 81 - min(81, N)
+
+        while k_empty != 0:
+            row = random.randint(0, 8)
+            col = random.randint(0, 8)
+            if grid[row][col] != ".":
+                grid[row][col] = "."
+                k_empty -= 1
+        return grid
+    else:
+        return []
+
+
+if __name__ == "__main__":
+    for fname in ["puzzle1.txt", "puzzle2.txt", "puzzle3.txt"]:
+        grid = read_sudoku(fname)
+        display(grid)
+        solution = solve(grid)
+        if not solution:
+            print(f"Puzzle {fname} can't be solved")
+        else:
+            display(solution)
