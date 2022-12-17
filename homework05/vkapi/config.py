cc84773652f89a5d1879f077e024832ad6aca1a8
Сема <112980469+Semen4ikk@users.noreply.github.com>
2022-12-17 18:36:44 +0300

    Add files via upload

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "vk1.a.gGIXuWM0EdQAT2gWTCIQagkOwWTS-vgRsxgpq_08Te7g0HItzSEr5tAFqHT3NyvsE8O6hK0NkIdjP1RVAt35Tr7_7wubWlwaEXr_gjrs09aXM1oCXTukYbJaiF0d97yZjVxm-Pl2AON4LDepugAFxLOm2WfozeyDMjugnElV86DFrb0dPv3Xh_rIYIW0eZxk",
     "version": "5.126",
 }
--- homework05/vkapi/session.py
+++ homework05/vkapi/session.py
@@ -1,3 +1,4 @@
+import time
 import typing as tp
 
 import requests
@@ -5,7 +6,21 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 
-class Session:
+
+class TimeoutHTTPAdapter(HTTPAdapter):
+    def __init__(self, *args, **kwargs):
+        self.timeout = kwargs["timeout"]
+        del kwargs["timeout"]
+        super().__init__(*args, **kwargs)
+
+    def send(self, request, **kwargs):
+        timeout = kwargs.get("timeout")
+        if timeout is None:
+            kwargs["timeout"] = self.timeout
+        return super().send(request, **kwargs)
+
+
+class Session(requests.Session):
     """
     Сессия.
 
@@ -22,10 +37,21 @@ class Session:
         max_retries: int = 3,
         backoff_factor: float = 0.3,
     ) -> None:
-        pass
+        super().__init__()
+        self.base_url = base_url
 
-    def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
-        pass
+        retry_strategy = Retry(
+            total=max_retries,
+            status_forcelist=[429, 500, 502, 503, 504],
+            method_whitelist=["HEAD", "GET", "OPTIONS", "POST"],
+            backoff_factor=backoff_factor,
+        )
+        adapter = TimeoutHTTPAdapter(max_retries=retry_strategy, timeout=timeout)
+        self.mount("https://", adapter)
+        self.mount("http://", adapter)
 
-    def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
-        pass
+    def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:  # type: ignore
+        return super().get(self.base_url + url, *args, **kwargs)
+
+    def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:  # type: ignore
+        return super().post(self.base_url + url, *args, **kwargs)
