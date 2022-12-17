2caf30c1406cc9f8715953888e402f2a248c281f
Сема <112980469+Semen4ikk@users.noreply.github.com>
2022-12-17 18:51:21 +0300

    Add files via upload

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -14,4 +14,26 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    pass
+    current = dt.datetime.now()
+    response = get_friends(user_id, fields=["bdate"])
+    ages = []
+    friend: tp.Dict[str, tp.Any]
+    for friend in response.items:  # type: ignore
+        if "bdate" not in friend:
+            continue
+        bdate = friend["bdate"]
+        if len(bdate) > 6:
+            bdate_tuple = dt.datetime.strptime(bdate, "%d.%m.%Y").timetuple()
+        else:
+            continue
+        if (current.month > bdate_tuple.tm_mon) or (
+            current.month == bdate_tuple.tm_mon and current.day > bdate_tuple.tm_mday
+        ):
+            age = current.year - bdate_tuple.tm_year
+        else:
+            age = current.year - bdate_tuple.tm_year - 1
+        ages.append(age)
+    if not ages:
+        return None
+    else:
+        return statistics.median(ages)
