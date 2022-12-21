12a438a42e107d1b23ef15f6480aac93ead935d2
Сема <112980469+Semen4ikk@users.noreply.github.com>
2022-12-21 15:32:34 +0300

    Update network.py

--- homework05/research/network.py
+++ homework05/research/network.py
@@ -5,27 +5,18 @@ import community as community_louvain
 import matplotlib.pyplot as plt
 import networkx as nx
 import pandas as pd
-
-from vkapi.friends import MutualFriends, get_friends, get_mutual
+from vkapi.friends import get_friends, get_mutual
 
 
 def ego_network(
     user_id: tp.Optional[int] = None, friends: tp.Optional[tp.List[int]] = None
 ) -> tp.List[tp.Tuple[int, int]]:
-    """
-    Построить эгоцентричный граф друзей.
-
-    :param user_id: Идентификатор пользователя, для которого строится граф друзей.
-    :param friends: Идентификаторы друзей, между которыми устанавливаются связи.
-    """
-    edges = []
-    if friends is None:
-        friends = get_friends(user_id).items  # type: ignore
-    mutual: tp.List[MutualFriends] = get_mutual(user_id, target_uids=friends)  # type: ignore
-    for i in mutual:
-        for common_friends in i["common_friends"]:
-            edges.append((common_friends, i["id"]))
-    return edges
+    friend = get_mutual(source_uid=user_id, target_uids=friends)
+    graph = []
+    for i in friend:
+        for j in i.get("common_friends", []):
+            graph += [(i["id"], j)]
+    return graph
 
 
 def plot_ego_network(net: tp.List[tp.Tuple[int, int]]) -> None:
